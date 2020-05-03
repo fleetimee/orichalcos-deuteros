015eb4c46111421e62be4eba22d89a5264a1a869
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-03 16:51:15 +0700

    initial commit

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card card-default">
+    <div class="card-header">
+        {{ isset($category) ? 'Edit category' : 'Create category' }} 
+    </div>
+    <div class="card-body">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="list-group">
+                    @foreach ($errors->all() as $error)
+                         <li class="list-group-item text-danger">
+                             {{ $error }}
+                         </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ isset($category) ? route('categories.update', $category->id) : route('categories.store') }}" method="POST">
+            @csrf
+            @if(isset($category))
+                @method('PUT')
+            @endif
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" class="form-control" value="{{ isset($category) ? $category->name : '' }}">
+            </div>
+            <div class="form-group">
+                <button class="btn btn-success">
+                    {{ isset($category) ? 'Update' : 'Create' }} 
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-end mb-2">
+        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
+    </div>
+    <div class="card card-default">
+        <div class="card-header">Categories</div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <th>Name</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>
+                                {{ $category->name }}
+                            </td>
+                            <td>
+                                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">
+                                Edit</a>
+                            </td>
+                        </tr> 
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+@endsection
