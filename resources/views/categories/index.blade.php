469e08e366a35cff09421afe1d392bdbc5026d7e
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-04 04:49:14 +0700

    Deleting categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -18,14 +18,35 @@
                             <td>
                                 {{ $category->name }}
                             </td>
-                            <td>
+                            <td class="float-right">
                                 <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">
                                 Edit</a>
+                                <button class="btn btn-danger btn-sm" onclick="handleDelete()">Delete</button>
                             </td>
                         </tr> 
                     @endforeach
                 </tbody>
             </table>
+
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="deleteModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
     </div>
     
