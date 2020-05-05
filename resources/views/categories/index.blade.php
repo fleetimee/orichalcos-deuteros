0a7bcd5f12aa313db2879434998f3a073356a1c4
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-05 08:59:35 +0700

    Add delete category function

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,59 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex justify-content-end mb-2">
-        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
-    </div>
-    <div class="card card-default">
-        <div class="card-header">Categories</div>
-        <div class="card-body">
-            <table class="table">
-                <thead>
-                    <th>Name</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    @foreach ($categories as $category)
-                        <tr>
-                            <td>
-                                {{ $category->name }}
-                            </td>
-                            <td class="float-right">
-                                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">
+<div class="d-flex justify-content-end mb-2">
+    <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
+</div>
+<div class="card card-default">
+    <div class="card-header">Categories</div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <th>Name</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach($categories as $category)
+                    <tr>
+                        <td>
+                            {{ $category->name }}
+                        </td>
+                        <td class="float-right">
+                            <a href="{{ route('categories.edit', $category->id) }}"
+                                class="btn btn-info btn-sm">
                                 Edit</a>
-                                <button class="btn btn-danger btn-sm" onclick="handleDelete()">Delete</button>
-                            </td>
-                        </tr> 
-                    @endforeach
-                </tbody>
-            </table>
+                            <button class="btn btn-danger btn-sm"
+                                onclick="handleDelete({{ $category->id }})">Delete</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 
-            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal fade" id="deleteModal" tabindex="-1" role="diaf" aria-labelledby="deleteModalLabel"
+            aria-hidden="true">
+            <form action="" method="POST" id="deleteCategoryForm">
+              @csrf
+              @method('DELETE')
                 <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="deleteModalLabel">Modal title</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go back</button>
+                            <button type="submit" class="btn btn-danger">Yes, Delete</button>
+                        </div>
                     </div>
-                    <div class="modal-body">
-                      ...
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                  </div>
                 </div>
-              </div>
+            </form>
         </div>
-@endsection
+    </div>
+    @endsection
 
-@section('scripts')
+    @section('scripts')
     <script>
-        function handleDelete() {
-            console.log('deleting')
+        function handleDelete(id) {
+            var form = document.getElementById('deleteCategoryForm')
+            form.action = '/categories/' + id
+            $('#deleteModal').modal('show')
         }
+
     </script>
-@endsection
+    @endsection
