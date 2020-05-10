ad4287b7e62f1e8e285ef1c2b99b016731f38ede
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-10 10:30:39 +0700

    add select2 di form tags

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -65,7 +65,7 @@
                 @if ($tags->count() > 0)
                     <div class="form-group">
                         <label for="tags">Tags</label>
-                        <select name="tags[]" id="tags" class="form-control" multiple>
+                        <select name="tags[]" id="tags" class="form-control tags-selector" multiple>
                             @foreach ($tags as $tag)
                                 <option value="{{ $tag->id }}"
                                 @if (isset($post))
@@ -93,13 +93,18 @@
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
     <script>
         flatpickr('#published_at', {
             enableTime: true
-        })
+        });
 
         document.addEventListener("trix-file-accept", function(event) {
             event.preventDefault();
+        });
+
+        $(document).ready(function() {
+            $('.tags-selector').select2();
         });
     </script>
 @endsection
@@ -114,6 +119,7 @@
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 
 @endsection
 
